edd3d1366443711545d35202d61703946945f2a4
Prafful <praffulsharma1230@gmail.com>
2023-07-28 15:06:16 +0530

    deployment vercel settings

--- hms_api/settings.py
+++ hms_api/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nv#_5_1(jm#+nvzwgf(t6)d0=+_obwr3##kj8^ii=p(f+4yp%y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
