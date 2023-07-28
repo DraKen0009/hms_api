75286e58ccb534430c0bba8cebd54a8167f94632
Prafful <praffulsharma1230@gmail.com>
2023-07-28 18:12:56 +0530

    changed settings.py for removing browsable apis

--- hms_api/settings.py
+++ hms_api/settings.py
@@ -134,6 +134,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
 }
 
 
@@ -141,4 +144,8 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+}
+
+REST_FRAMEWORK = {
+
 }
